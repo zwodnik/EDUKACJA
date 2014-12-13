a52abd25ffe4c58679debcb775783ab72a7627ab
Jechbeat <jechbeatdev@gmail.com>
2014-12-13 09:41:52 +0100

    hciv

--- Edukejszyn/Edukejszyn/Edukejszyn.Shared/Logika.cs
+++ Edukejszyn/Edukejszyn/Edukejszyn.Shared/Logika.cs
@@ -17,11 +17,34 @@ namespace Edukejszyn
         public Kategoria kategoria;
         public int punkty = 0;
         public int index = 0;
+        public int[] randomik = new int[5];
 
         public Logika()
         {
         }
 
+        public void Losowanie()
+        {
+              for(int i = 0; i < 5; i++)
+            {
+                randomik[i] = -1;
+               
+            }
+            Random random = new Random();
+            for(int i = 0; i < 5; i++)
+            {
+               
+                randomik[i] = random.Next(0, 5);
+                for (int j = 0; j < 5; j++)
+                {
+                    if (randomik[i] == randomik[j] && i != j)
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+        }
         
     }
 }
